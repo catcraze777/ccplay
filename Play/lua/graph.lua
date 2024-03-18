7e78dcea50caa1017174ae2536c8d792b06a798c
catcraze777 <52767118+catcraze777@users.noreply.github.com>
2024-03-17 22:07:25 -0700

    Hide Graph PB if doesn't exist.
    Graph now hides the PB graph if a PB doesn't exist and makes the other bars larger to compensate for the empty space.

--- Play/lua/graph.lua
+++ Play/lua/graph.lua
@@ -3,6 +3,7 @@ local property = require "lua/property_funcs"
 local Object = require "object"
 local Objects = require "objects"
 local utils = require "utils"
+local main_state = require "main_state"
 
 local Graph = Object.new()
 function Graph.image()
@@ -62,6 +63,8 @@ function Graph.destination()
 			{time = 375 + 500, x = graph_x + 16 * direction, y = graph_y, a = 192},
 			{time = 500 + 500, x = graph_x, y = graph_y, a = 255},
 		}})
+		
+	-- Grade lines
 	table.insert(t, {id = "graph_max", loop = 1250, op = {2012}, dst = { 
 			{time = 0 + 1000, x = graph_x, y = graph_y + 778, w = 298, h = 32, a = 0},
 			{time = 250 + 1000, x = graph_x, y = graph_y + 778, a = 255},
@@ -111,54 +114,96 @@ function Graph.destination()
 			{time = 0 + 1300, x = graph_x, y = graph_y + 513, w = 298, h = 32, a = 0},
 			{time = 250 + 1300, x = graph_x, y = graph_y + 513, a = 255},
 		}})
-	table.insert(t, {id = "graph-score", loop = 1650, op = {2012, 2031}, dst = { 
+	
+	-- Target graph & Personal Best
+		-- Draw these if personal best exists.
+	local_func = function() return (main_state.number(150) > 0) and skin_config.option["Graph Personal Best Only"] == 2031 end
+	table.insert(t, {id = "graph-score", loop = 1650, op = {2012, 2031}, draw = local_func, dst = { 
 			{time = 0 + 1400, x = graph_x + 67, y = graph_y, w = 66, h = 810, a = 0},
 			{time = 250 + 1400, x = graph_x + 67, y = graph_y, a = 255},
 		}})
-	table.insert(t, {id = "graph-pb-ghost", loop = 1650, op = {2012, 2031}, dst = { 
+	table.insert(t, {id = "graph-pb-ghost", loop = 1650, op = {2012, 2031}, draw = local_func, dst = { 
 			{time = 0 + 1400, x = graph_x + 67 + 77, y = graph_y, w = 66, h = 0, a = 0},
 			{time = 250 + 1400, x = graph_x + 67 + 77, y = graph_y, h = 810, a = 255},
 		}})
-	table.insert(t, {id = "graph-pb", loop = 1650, op = {2012, 2031}, dst = { 
+	table.insert(t, {id = "graph-pb", loop = 1650, op = {2012, 2031}, draw = local_func, dst = { 
 			{time = 0 + 1400, x = graph_x + 67 + 77, y = graph_y, w = 66, h = 810, a = 0},
 			{time = 250 + 1400, x = graph_x + 67 + 77, y = graph_y, a = 255},
 		}})
-	table.insert(t, {id = "graph-target-ghost", loop = 1650, op = {2012, 2031}, dst = { 
+	table.insert(t, {id = "graph-target-ghost", loop = 1650, op = {2012, 2031}, draw = local_func, dst = { 
 			{time = 0 + 1400, x = graph_x + 67 + 154, y = graph_y, w = 66, h = 0, a = 0},
 			{time = 250 + 1400, x = graph_x + 67 + 154, y = graph_y, h = 810, a = 255},
 		}})
-	table.insert(t, {id = "graph-target", loop = 1650, op = {2012, 2031}, dst = { 
+	table.insert(t, {id = "graph-target", loop = 1650, op = {2012, 2031}, draw = local_func, dst = { 
 			{time = 0 + 1400, x = graph_x + 67 + 154, y = graph_y, w = 66, h = 810, a = 0},
 			{time = 250 + 1400, x = graph_x + 67 + 154, y = graph_y, a = 255},
 		}})
-	table.insert(t, {id = "graph-score", loop = 1650, op = {2012, 2032}, dst = { 
+		-- Draw these if personal best does not exist.
+	local_func = function() return (main_state.number(150) == 0) and skin_config.option["Graph Personal Best Only"] == 2031 end
+	table.insert(t, {id = "graph-score", loop = 1650, op = {2012, 2031}, draw = local_func, dst = { 
 			{time = 0 + 1400, x = graph_x + 67, y = graph_y, w = 100, h = 810, a = 0},
 			{time = 250 + 1400, x = graph_x + 67, y = graph_y, a = 255},
 		}})
-	table.insert(t, {id = "graph-pb-ghost", loop = 1650, op = {2012, 2032}, dst = { 
+	table.insert(t, {id = "graph-target-ghost", loop = 1650, op = {2012, 2031}, draw = local_func, dst = { 
 			{time = 0 + 1400, x = graph_x + 67 + 120, y = graph_y, w = 100, h = 0, a = 0},
 			{time = 250 + 1400, x = graph_x + 67 + 120, y = graph_y, h = 810, a = 255},
 		}})
-	table.insert(t, {id = "graph-pb", loop = 1650, op = {2012, 2032}, dst = { 
+	table.insert(t, {id = "graph-target", loop = 1650, op = {2012, 2031}, draw = local_func, dst = { 
 			{time = 0 + 1400, x = graph_x + 67 + 120, y = graph_y, w = 100, h = 810, a = 0},
 			{time = 250 + 1400, x = graph_x + 67 + 120, y = graph_y, a = 255},
 		}})
-	table.insert(t, {id = "graph_score_best_text", loop = 1950, op = {2012}, dst = { 
+		
+	-- Personal Best only
+		-- Draw these if personal best exists.
+	local_func = function() return (main_state.number(150) > 0) and skin_config.option["Graph Personal Best Only"] == 2032 end
+	table.insert(t, {id = "graph-score", loop = 1650, op = {2012, 2032}, draw = local_func, dst = { 
+			{time = 0 + 1400, x = graph_x + 67, y = graph_y, w = 100, h = 810, a = 0},
+			{time = 250 + 1400, x = graph_x + 67, y = graph_y, a = 255},
+		}})
+	table.insert(t, {id = "graph-pb-ghost", loop = 1650, op = {2012, 2032}, draw = local_func, dst = { 
+			{time = 0 + 1400, x = graph_x + 67 + 120, y = graph_y, w = 100, h = 0, a = 0},
+			{time = 250 + 1400, x = graph_x + 67 + 120, y = graph_y, h = 810, a = 255},
+		}})
+	table.insert(t, {id = "graph-pb", loop = 1650, op = {2012, 2032}, draw = local_func, dst = { 
+			{time = 0 + 1400, x = graph_x + 67 + 120, y = graph_y, w = 100, h = 810, a = 0},
+			{time = 250 + 1400, x = graph_x + 67 + 120, y = graph_y, a = 255},
+		}})
+		-- Draw these if personal best does not exist.
+	local_func = function() return (main_state.number(150) == 0) and skin_config.option["Graph Personal Best Only"] == 2032 end
+	table.insert(t, {id = "graph-score", loop = 1650, op = {2012, 2032}, draw = local_func, dst = { 
+			{time = 0 + 1400, x = graph_x + 67 + 35, y = graph_y, w = 150, h = 810, a = 0},
+			{time = 250 + 1400, x = graph_x + 67 + 35, y = graph_y, a = 255},
+		}})
+		
+	-- Score text values.
+		-- Personal Best exists.
+	table.insert(t, {id = "graph_score_best_text", loop = 1950, op = {2012}, draw = function() return main_state.number(150) > 0 end, dst = { 
 			{time = 0 + 1700, x = graph_x + 75, y = graph_y + 5, w = 114, h = 25, a = 0},
 			{time = 250 + 1700, a = 255},
 		}})
-	table.insert(t, {id = "graph_score_best_value", loop = 1950, op = {2012}, dst = { 
+	table.insert(t, {id = "graph_score_best_value", loop = 1950, op = {2012}, draw = function() return main_state.number(150) > 0 end, dst = { 
 			{time = 0 + 1700, x = graph_x + 75 + 120, y = graph_y - 1 + 5, w = 20, h = 28, a = 0},
 			{time = 250 + 1700, a = 255},
 		}})
-	table.insert(t, {id = "graph_score_target_text", loop = 2150, op = {2012, 2031}, dst = { 
+	table.insert(t, {id = "graph_score_target_text", loop = 2150, op = {2012, 2031}, draw = function() return (main_state.number(150) > 0) and skin_config.option["Graph Personal Best Only"] == 2031 end, dst = { 
 			{time = 0 + 1900, x = graph_x + 75, y = graph_y + 5 + 30, w = 114, h = 25, a = 0},
 			{time = 250 + 1900, a = 255},
 		}})
-	table.insert(t, {id = "graph_score_target_value", loop = 2150, op = {2012, 2031}, dst = { 
+	table.insert(t, {id = "graph_score_target_value", loop = 2150, op = {2012, 2031}, draw = function() return (main_state.number(150) > 0) and skin_config.option["Graph Personal Best Only"] == 2031 end, dst = { 
 			{time = 0 + 1900, x = graph_x + 75 + 120, y = graph_y - 1 + 5 + 30, w = 20, h = 28, a = 0},
 			{time = 250 + 1900, a = 255},
 		}})
+		-- Personal best does not exist.
+	table.insert(t, {id = "graph_score_target_text", loop = 2150, op = {2012, 2031}, draw = function() return (main_state.number(150) == 0) and skin_config.option["Graph Personal Best Only"] == 2031 end, dst = { 
+			{time = 0 + 1700, x = graph_x + 75, y = graph_y + 5, w = 114, h = 25, a = 0},
+			{time = 250 + 1700, a = 255},
+		}})
+	table.insert(t, {id = "graph_score_target_value", loop = 2150, op = {2012, 2031}, draw = function() return (main_state.number(150) == 0) and skin_config.option["Graph Personal Best Only"] == 2031 end, dst = { 
+			{time = 0 + 1700, x = graph_x + 75 + 120, y = graph_y - 1 + 5, w = 20, h = 28, a = 0},
+			{time = 250 + 1700, a = 255},
+		}})
+		
+	-- Color flashes for PB, Target, and A/AA/AAA reaches.
 	table.insert(t, {id = -111, loop = 500, timer = 348, op = {2012}, blend = 2, dst = { 
 			{time = 0, x = graph_x, y = graph_y, w = 298, h = 810, a = 0, r = 100, g = 100, b = 100},
 			{time = 100, a = 255},
@@ -179,7 +224,7 @@ function Graph.destination()
 			{time = 100, a = 255},
 			{time = 500, a = 0},
 		}})
-	table.insert(t, {id = -111, loop = 500, timer = 351, op = {2012}, blend = 2, dst = { 
+	table.insert(t, {id = -111, loop = 500, timer = 351, op = {2012}, draw = function() return (main_state.number(150) > 0) end, blend = 2, dst = { 
 			{time = 0, x = graph_x, y = graph_y, w = 298, h = 810, a = 0, r = 0, g = 128, b = 0},
 			{time = 100, a = 255},
 			{time = 500, a = 0},
